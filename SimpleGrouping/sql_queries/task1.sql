e003b384ff81ed931a2b9d74d2b98764e68bffa4
sgorgun <sergei.gorgun@gmail.com>
2024-03-09 13:38:40 +0200

    Ready

--- SimpleGrouping/sql_queries/task1.sql
+++ SimpleGrouping/sql_queries/task1.sql
@@ -1,1 +1,9 @@
-﻿
+﻿SELECT
+    COUNT(person_id) AS customer_count,
+    discount
+FROM
+    customer
+GROUP BY
+    discount
+ORDER BY
+    discount;
--- SimpleGrouping/sql_queries/task3.sql
+++ SimpleGrouping/sql_queries/task3.sql
@@ -1,1 +1,12 @@
-﻿
+﻿WITH OrderDiscounts AS (
+    SELECT
+        customer_order_id, SUM((price - price_with_discount) * product_amount) AS total_order_discount
+    FROM
+        order_details
+    GROUP BY
+        customer_order_id
+)
+SELECT
+    MAX(total_order_discount) AS max_order_discount
+FROM
+    OrderDiscounts;
